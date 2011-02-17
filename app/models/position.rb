019b07b679e68d51332fecf6ad1b7834a5d87396
Yaacob Noor <yaacob@teknikpadu.com.my>
2011-02-18 02:13:43 +0800

    Updated Staff vs Position Assignment & sorted drop down list filtering

--- app/models/position.rb
+++ app/models/position.rb
@@ -3,20 +3,23 @@ class Position < ActiveRecord::Base
   has_many :subordinates, :class_name => 'Position', :foreign_key => 'parent_id'
   belongs_to :bosses, :class_name => 'Position', :foreign_key => 'parent_id'
   belongs_to :staffgrade, :class_name => 'Staffgrade', :foreign_key => 'staffgrade_id'
- # belongs_to :staffid, :class_name => 'Leaveforstaff', :foreign_key => 'staff_id'
-  
-  has_one :staff
+  belongs_to :staff
+
   validates_uniqueness_of :positioncode
   validates_presence_of :positioncode
   
   def self.find_main
     Position.find(:all, :condition => ['parent_id IS NULL'])
-    @staff = Staff.find(@staff.position_id);
+    @staff = Staff.find(@staff.position.staff_id);
    # @leaveforstaff = Leaveforstaff.find(@leaveforstaff.approval1_id);
   end
   
   def position_with_code
     "#{positioncode}  #{positionname}"
+  end
+  
+  def people_assigned
+    Position.find(:all, :select => "staff_id", :conditions => ["staff_id IS NOT ?", nil]).map(&:staff_id)
   end
   
   
