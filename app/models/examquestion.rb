33a870fe11f8fec35990b020b33237e659170ea2
MaxCobMara <maxcobmara@gmail.com>
2012-09-16 17:17:36 +0800

    Exam Question Overhaul

--- app/models/examquestion.rb
+++ app/models/examquestion.rb
@@ -6,6 +6,7 @@ class Examquestion < ActiveRecord::Base
   belongs_to :subject
   belongs_to :programme
   belongs_to :topic
+  has_and_belongs_to_many :exammakers
   
   has_attached_file :diagram,
                     :url => "/assets/examquestions/:id/:style/:basename.:extension",
@@ -15,13 +16,39 @@ class Examquestion < ActiveRecord::Base
                     
   validates_presence_of :subject_id, :questiontype, :question, :answer, :marks, :qstatus
   
+  
   has_many :examsubquestions, :dependent => :destroy
   accepts_nested_attributes_for :examsubquestions, :reject_if => lambda { |a| a[:question].blank? }
   
   has_many :exammcqanswers, :dependent => :destroy
   accepts_nested_attributes_for :exammcqanswers, :reject_if => lambda { |a| a[:answer].blank? }
   
-  has_and_belongs_to_many :exammakers
+  
+  
+  def status_workflow
+    flow = Array.new
+        if qstatus == nil || qstatus == "New"
+          flow << "New" << "Submit"
+        elsif qstatus == "Submit" || qstatus == "Editing"
+          flow << "Editing" << "Ready For Approval" 
+        elsif qstatus == "Re-Edit"
+          flow << "Re-Edit"  << "For Approval"
+        elsif qstatus == "Ready For Approval" || qstatus == "For Approval"
+          flow << "Re-Edit" << "Approved" << "Rejected"
+        elsif qstatus == "Approved"
+           flow << "Approved"
+        else
+        end
+    flow
+  end
+  
+  def question_editor
+    sibpos = creator.position.parent.sibling_ids
+    sibs   = Position.find(:all, :select => "staff_id", :conditions => ["id IN (?)", sibpos]).map(&:staff_id)
+    applicant = Array(creator_id)
+    sibs - applicant
+  end
+  
   
   def self.search(search)
      if search
@@ -124,33 +151,25 @@ class Examquestion < ActiveRecord::Base
            end
       end
       
-       def editor_details 
-             suid = editor_id.to_a
-             exists = Staff.find(:all, :select => "id").map(&:id)
-             checker = suid & exists     
-
-             if editor_id == nil
-                "" 
-              elsif checker == []
-                "Staff No Longer Exists" 
-             else
-               editor.mykad_with_staff_name
-             end
-        end
+  def editor_details         
+    if editor.blank?
+      "None Assigned"
+    elsif editor_id?
+      editor.mykad_with_staff_name
+    else 
+      "None Assigned"
+    end
+  end
+  
+  def approver_details
+    if approver.blank? 
+      "None Assigned"
+    else 
+      approver.name
+    end
+  end
         
-        def approver_details 
-               suid = approver_id.to_a
-               exists = Staff.find(:all, :select => "id").map(&:id)
-               checker = suid & exists     
 
-               if approver_id == nil
-                  "" 
-                elsif checker == []
-                  "Staff No Longer Exists" 
-               else
-                 approver.name
-               end
-          end
    
   
 end
