90cf573f8f122b8f48a877e2761d7adf8ea4cbf8
MaxCobMara <maxcobmara@gmail.com>
2013-05-13 19:56:10 +0800

    Finish layout and routesand install devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Catechumen::Application.routes.draw do
   
+  root to: 'static_pages#home'
   devise_for :users
 
-  root to: 'static_pages#home'
+  
   match '/help', to: 'static_pages#help'
   match '/about', to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,47 +8,26 @@ describe "Static Pages" do
     before { visit root_path } 
     
     it { should have_selector('h1', text: 'Catechumen') }
-    it { should have_selector 'title', text: "Catechumen" }
+    it { should have_selector('title', text: full_title('')) }
     it { should_not have_selector 'title', text: '| Home' }    
   end
   
   describe "Help page" do
-    it "should have the h1 'Help'" do
-      visit help_path
-      page.should have_content('Help')
-    end
-    
-    it "should have the title 'Home'" do
-      visit help_path
-      page.should have_selector('title',
-                        :text => "Catechumen | Help")
-    end
+    before { visit help_path }
+    it { should have_selector('h1',    text: 'Help') }
+    it { should have_selector('title', text: full_title('Help')) }
   end
   
   describe "About page" do
-    it "should have the h1 'About Catechumen'" do
-      visit about_path
-      page.should have_content('About Catechumen')
-    end
-    
-    it "should have the title 'About'" do
-      visit about_path
-      page.should have_selector('title',
-                        :text => "Catechumen | About")
-    end
+    before { visit about_path }
+    it { should have_selector('h1',    text: 'About') }
+    it { should have_selector('title', text: full_title('About')) }
   end
   
   describe "Contact page" do
-    it "should have the h1 'Contact Us'" do
-      visit contact_path
-      page.should have_content('Contact Us')
-    end
-    
-    it "should have the title 'Contact'" do
-      visit contact_path
-      page.should have_selector('title',
-                        :text => "Catechumen | Contact")
-    end
+    before { visit contact_path }
+    it { should have_selector('h1',    text: 'Contact') }
+    it { should have_selector('title', text: full_title('Contact')) }
   end
   
 end
