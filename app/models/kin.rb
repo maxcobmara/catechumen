174d8630a828328b2da0a901626b834737fc52e5
Shima <shima@MacBook-2.local>
2014-02-08 18:57:09 +0800

    Fix for staff info - 'name' & staff's kin 'name'-draft - prev comment (data dictionary) not yet applied

--- app/models/kin.rb
+++ app/models/kin.rb
@@ -1,7 +1,14 @@
 class Kin < ActiveRecord::Base
+  before_save  :titleize_name
+
+  def titleize_name
+    self.name = name.titleize
+  end
   belongs_to :staff
   belongs_to :student
-  
+  validates_numericality_of :mykadno, :allow_blank => true
+  validates_length_of       :mykadno, :is =>12, :allow_blank => true
+  validates_format_of :name, :with => /^[a-zA-Z'`\/\. ]+$/, :message => I18n.t('activerecord.errors.messages.illegal_char') #add unwanted chars between bracket
   KTYPE = [
           #  Displayed       stored in db
           [ "Isteri",1 ],
