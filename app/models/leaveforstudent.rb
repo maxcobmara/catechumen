2130d388513c7f60b7a3444eedf4a27460e1a310
Nurhashimah <nurhashimah77@gmail.com>
2015-01-08 16:21:39 +0800

    #588 Leave for student (tambahkan no tel penjaga) - revised

--- app/models/leaveforstudent.rb
+++ app/models/leaveforstudent.rb
@@ -5,7 +5,6 @@ class Leaveforstudent < ActiveRecord::Base
   validates_presence_of :student_id, :leavetype, :leave_startdate, :leave_enddate
   validates_numericality_of :telno
   
-  
   def self.find_main
     Student.find(:all, :condition => ['student_id IS NULL'])
   end
@@ -21,29 +20,27 @@ class Leaveforstudent < ActiveRecord::Base
       errors.add(:end_date, "Your leave must begin before it ends") if leave_enddate < leave_startdate || leave_startdate < DateTime.now
     end
   end
-  
- 
+
  STUDENTLEAVETYPE = [
         #  Displayed       stored in db
-        [ "Weekend Day","Weekend Day" ],
-        [ "Weekend Overnight","Weekend Overnight" ],
-        [ "Emergency","Emergency" ],
-        [ "Cuti Perayaan","Cuti Perayaan" ],
-        [ "Mid Term Break","Mid Term Break" ],
-        [ "End of Semester","End of Semester" ]
+        [ I18n.t("leaveforstudent.weekend_day"),"Weekend Day" ],
+        [ I18n.t("leaveforstudent.weekend_overnight"),"Weekend Overnight" ],
+        [ I18n.t("leaveforstudent.emergency"),"Emergency" ],
+        [ I18n.t("leaveforstudent.festive_leave"),"Cuti Perayaan" ],
+        [ I18n.t("leaveforstudent.midterm_break"),"Mid Term Break" ],
+        [ I18n.t("leaveforstudent.end_of_semester"),"End of Semester" ]
   ]
-  
-  
-def self.search(search)
+
+  def self.search(search)
      if search
        @leaveforstudent = Leaveforstudent.find(:all, :conditions => ['leavetype LIKE ?' , "%#{search}%"])
      else
       @leaveforstudent = Leaveforstudent.find(:all)
      end
-end
+  end
 
-#<07/10/2011 - Shaliza fixed for error when staff no longer exists>
-def approver_details 
+  #<07/10/2011 - Shaliza fixed for error when staff no longer exists>
+  def approver_details 
       suid = staff_id.to_a
       exists = Staff.find(:all, :select => "id").map(&:id)
       checker = suid & exists     
@@ -55,10 +52,10 @@ def approver_details
       else
         staff.name
       end
-end
+  end
 
-#<18/10/2011 - Shaliza fixed for error when student no longer exists>
-def student_details 
+  #<18/10/2011 - Shaliza fixed for error when student no longer exists>
+  def student_details 
       suid = student_id.to_a
       exists = Student.find(:all, :select => "id").map(&:id)
       checker = suid & exists     
@@ -70,6 +67,6 @@ def student_details
       else
         student.formatted_mykad_and_student_name
       end
-end
+  end
 
 end
