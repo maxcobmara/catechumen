1f37aca297accbcdc52b4580146c509206c1d126
Yaacob Noor <maxcobmara@gmail.com>
2015-01-14 00:36:21 +0800

    gem list

--- app/models/login.rb
+++ app/models/login.rb
@@ -17,12 +17,12 @@ class Login < ActiveRecord::Base
   validates_length_of       :email,    :within => 3..100 #r@a.wk
   validates_uniqueness_of   :email
   validates_format_of       :email,    :with => Authentication.email_regex, :message => Authentication.bad_email_message
-  
+
   #validates_presence_of     :icno,     :within => 3..100         #hide 15July2013
   validates_numericality_of :icno,     :only_integer => true      #added 15July2013
   validates_length_of       :icno,     :is => 12, :message => "MyKad no is 12 characters"
   validates_uniqueness_of   :icno,     :message => "Your MyKad no already has a registered account"
-  
+
   validates_presence_of     :isstaff, :message => "Please select staff or student"    #added 15July2013
   has_and_belongs_to_many :roles
   belongs_to :staff
@@ -31,10 +31,10 @@ class Login < ActiveRecord::Base
   # HACK HACK HACK -- how to do attr_accessible from here?
   # prevents a user from submitting a crafted form that bypasses activation
   # anything else you want your user to change should be added here.
-  #attr_accessible :login, :email, :name, :password, :password_confirmation, :isstaff, :staff_id, 
+  #attr_accessible :login, :email, :name, :password, :password_confirmation, :isstaff, :staff_id,
                   #:student_id
-                  
-  cattr_accessor :current_login
+
+  attr_accessor :current_login
 
   def role_symbols
     roles.map do |role|
@@ -45,7 +45,7 @@ class Login < ActiveRecord::Base
 
   # Authenticates a user by their login name and unencrypted password.  Returns the user or nil.
   #
-  # uff.  this is really an authorization, not authentication routine.  
+  # uff.  this is really an authorization, not authentication routine.
   # We really need a Dispatch Chain here or something.
   # This will also let us return a human error message.
   #
@@ -62,7 +62,7 @@ class Login < ActiveRecord::Base
   def email=(value)
     write_attribute :email, (value ? value.downcase : nil)
   end
-  
+
   def login_nama
     stid = Array(staff_id)
     suid = Array(student_id)
@@ -70,7 +70,7 @@ class Login < ActiveRecord::Base
     suexists = Student.find(:all, :select => "id").map(&:id)
     staffchecker = stid & stexists
     studentchecker = suid & suexists
-    
+
       if student_id == nil && staff_id == nil
         ""
       elsif staff_id == nil && studentchecker == []
@@ -78,16 +78,16 @@ class Login < ActiveRecord::Base
       elsif isstaff == true && staffchecker == []
         "Staff No Longer Exists"
       elsif isstaff == false
-        " #{student.name} + (Student)"   
+        " #{student.name} + (Student)"
       else
         staff.name
-      end 
+      end
   end
-  
+
   def assigned_staff
     Login.find(:all, :select => "staff_id", :conditions => ["staff_id IS NOT ?", nil]).map(&:staff_id)
   end
-  
+
   named_scope :all
   named_scope :approval,  :conditions =>  ["student_id IS? AND staff_id IS ?", nil, nil]
   named_scope :staff,     :conditions =>  ["student_id IS? AND staff_id IS NOT ?", nil, nil]
@@ -100,7 +100,7 @@ class Login < ActiveRecord::Base
     {:scope => "staff",     :label => "Staff"},
     {:scope => "student",   :label => "Student"}
     ]
-  
 
-  
+
+
 end
