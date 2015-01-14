a0ba1bcb12933978068ec7a938f9fa1a0e5aaf70
Yaacob Noor <maxcobmara@gmail.com>
2015-01-14 15:26:31 +0800

    fix currentlogin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,14 +4,14 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
-  
-  
+
+
   include AuthenticatedSystem
    before_filter :set_locale
    before_filter :get_pages_for_tabs
    before_filter :put_current_login_into_model
-   before_filter { |c| Authorization.current_user = Login.current_login}
-   
+   before_filter { |c| Authorization.current_user = Login.current_user}
+
 
     def get_pages_for_tabs
       if logged_in?
@@ -21,7 +21,7 @@ class ApplicationController < ActionController::Base
       end
 
     end
-    
+
     protected
 
       def current_user
@@ -47,22 +47,22 @@ class ApplicationController < ActionController::Base
         I18n.load_path -= [locale_path]
         I18n.locale = session[:locale] = I18n.default_locale
       end
-      
+
       def put_current_login_into_model
         @login = Login.find_by_id(session[:login_id])
         if @login
           Login.current_login = @login
         end
       end
-      
+
       def permission_denied
         flash[:error] = "Sorry, your user permissions do not allow you to access this information"
         redirect_to root_url
       end
-      
+
       def late_to_be_approved
         Attendance.count(:all, :conditions => ["approve_id=? AND approvestatus IS ?", Login.current_login.staff_id, nil])
       end
-    
-    
+
+
 end
