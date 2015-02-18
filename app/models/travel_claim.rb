369dfa6fe87f45885021289f8f5f2200eafb5a4e
Nurhashimah <nurhashimah77@gmail.com>
2015-02-18 22:24:07 +0800

    Misc fixes for Travel Claims

--- app/models/travel_claim.rb
+++ app/models/travel_claim.rb
@@ -18,7 +18,7 @@ class TravelClaim < ActiveRecord::Base
   has_many :travel_claim_allowances, :dependent => :destroy
   accepts_nested_attributes_for :travel_claim_allowances, :reject_if => lambda { |a| a[:amount].blank? }, :allow_destroy =>true
   
-  validates_uniqueness_of :claim_month, :scope => :staff_id, :message => "You already have claims for this month"
+  validates_uniqueness_of :claim_month, :scope => :staff_id, :message => I18n.t('claim.claim_exist')
   validate :accommodations_must_exist_for_lodging_hotel_claims
   
   attr_accessor :edittype
