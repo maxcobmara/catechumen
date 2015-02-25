8883f904ef5bc6ab455a6b85f0f72fdc8597ad5c
Nurhashimah <nurhashimah77@gmail.com>
2015-02-26 04:55:31 +0800

    Lesson Plan & Report - Editable location fixed + Show (broken edit link fixed)

--- app/controllers/lesson_plans_controller.rb
+++ app/controllers/lesson_plans_controller.rb
@@ -70,8 +70,11 @@ class LessonPlansController < ApplicationController
     @lesson_plan = LessonPlan.find(params[:id])
     newlocation = params[:new_location]
     if newlocation!=nil
-      scheduleid = params[:lesson_plan][:schedule]
-      scheduleid = @lesson_plan.schedule if scheduleid==nil
+      if @lesson_plan.is_submitted == true        #after submission -> schedule confirm exist, even validation - schedule must exist upon save
+        scheduleid = @lesson_plan.schedule if scheduleid==nil
+      else
+        scheduleid = params[:lesson_plan][:schedule]
+      end
       if scheduleid!=nil
         schedule = WeeklytimetableDetail.find(scheduleid) 
         schedule.location_desc = newlocation
