e0a1889a5b856c7ca25e8803d342b007c8270c48
Nurhashimah <nurhashimah77@gmail.com>
2015-03-03 16:04:41 +0800

    Stationery - validation added : quantity & price per unit must be within 1-9

--- app/models/stationery_add.rb
+++ app/models/stationery_add.rb
@@ -2,6 +2,8 @@ class StationeryAdd < ActiveRecord::Base
   belongs_to :stationery
   
   validates_presence_of :document, :unitcost, :received, :quantity #, :lpono
+  validates_format_of      :quantity, :with => /[1-9]/, :message => I18n.t('activerecord.errors.messages.invalid')
+  validates_format_of      :unitcost, :with => /[1-9]/, :message => I18n.t('activerecord.errors.messages.invalid')
   
   #attr_accessible :line_item_value
   attr_accessor :total
--- app/models/stationery_use.rb
+++ app/models/stationery_use.rb
@@ -4,4 +4,5 @@ class StationeryUse < ActiveRecord::Base
  belongs_to :receiver,       :class_name => 'Staff', :foreign_key => 'receivedby'
  
  validates_presence_of :issuedby, :receivedby, :issuedate, :quantity
+ validates_format_of      :quantity, :with => /[1-9]/, :message => I18n.t('activerecord.errors.messages.invalid')
 end
