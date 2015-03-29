0ad20196c3545d891800d6e0049dfc0dcb29495b
Yaacob Noor <maxcobmara@gmail.com>
2015-03-29 09:28:33 +0800

    update boot script

--- boot_production.sh
+++ boot_production.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -v
 set -o verbose
+git checkout master
+git pull origin master
 git branch -a
 git fetch origin production
 git checkout production
-ruby script/server -e production
+git pull origin production
+ruby script/server -p 3000 -e production
