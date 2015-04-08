c723fab3fd06b3fd637a6299351fbcfff821a86c
Nurhashimah <nurhashimah77@gmail.com>
2015-04-08 13:25:38 +0800

    Equery - Maklumat Perjawatan - Search fields revised+result(show pg) revised & fixed (totals count row)

--- app/controllers/staffsearch2s_controller.rb
+++ app/controllers/staffsearch2s_controller.rb
@@ -4,7 +4,7 @@ class Staffsearch2sController < ApplicationController
   end
 
   def create
-    #raise params.inspect
+   #raise params.inspect
     @staffsearch2 = Staffsearch2.new(params[:staffsearch2])
     if @staffsearch2.save
       #flash[:notice] = "Successfully created staffsearch2."
@@ -17,12 +17,17 @@ class Staffsearch2sController < ApplicationController
   def show
     @staffsearch2 = Staffsearch2.find(params[:id])
     @ppp = params[:ppp]
-    @positions = Position.find(:all, :conditions=>['staff_id IN(?)', @staffsearch2.staffs.map(&:id)], :order => :ancestry_depth)  #Position.find(:all, :order => :ancestry_depth)
+    #keywords, :position, :staff_grade, :position2, :position3
+    if @staffsearch2.keywords.blank? && @staffsearch2.position.blank? && @staffsearch2.staff_grade.blank? && @staffsearch2.position2.blank? && @staffsearch2.position3.blank?
+      @positions = []
+    else
+      @positions = Position.find(:all, :conditions=>['staff_id IN(?) AND name!=?', @staffsearch2.staffs.map(&:id), 'ICMS Vendor Admin'], :order => :ancestry_depth)  #Position.find(:all, :order => :ancestry_depth)  
+    end
 
     if @ppp=='2'
       @positions2=[]
       #BELOW SAME AS : unless position.staffgrade.blank? && position.postinfo_id.blank? 
-      @positions_raw = Position.find(:all, :conditions=> ['staffgrade_id IS NOT NULL AND postinfo_id IS NOT NULL AND staff_id IN(?)', @staffsearch2.staffs.map(&:id)]) 
+      @positions_raw = Position.find(:all, :conditions=> ['staffgrade_id IS NOT NULL AND postinfo_id IS NOT NULL AND staff_id IN(?) AND name!=?', @staffsearch2.staffs.map(&:id), 'ICMS Vendor Admin']) 
       @positions_raw.group_by{|x|x.staffgrade.name.scan(/[a-zA-Z]+|[0-9]+/)[1].to_i}.sort.reverse!.each do |staffgrade2, positions_of_grade_no|
          positions_of_grade_no.group_by{|x|x.staffgrade.name.scan(/[a-zA-Z]+|[0-9]+/)[0]}.sort.reverse!.each do |staffgrade, positions_by_grade|
            @positions2.concat(positions_by_grade)
@@ -35,5 +40,6 @@ class Staffsearch2sController < ApplicationController
     else
       render :layout => 'report'
     end
+    
   end
 end
