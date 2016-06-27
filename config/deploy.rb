22de3e7cc519d941695fd534e789ba4217375c58
Nurhashimah <nurhashimah77@gmail.com>
2016-06-27 11:58:38 +0800

    cap revised

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,13 +6,7 @@ require 'capistrano/ext/multistage'
 set :application, "catechumen"
 set :repository,  "git@github.com:maxcobmara/catechumen.git"
 
-set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-
-#role :web, "106.187.51.114"                          # Your HTTP server, Apache/etc
-#role :app, "106.187.51.114"                          # This may be the same as your `Web` server
-#role :db,  "106.187.51.114", :primary => true # This is where Rails migrations will run
-#role :db,  "your slave db-server here"
+set :scm, :git 
 
 set :user, "nurhashimah"
 set :stages, ["staging", "production"]
@@ -23,27 +17,28 @@ set :rails_env, "production"
 set :deploy_via, :copy
 set :ssh_options, {:forward_agent => true, :port => 4321}
 
-set :rvm_type, :user                     # Defaults to: :auto
-set :rvm_ruby_version, '1.8.7-p374@catechumen'      # Defaults to: 'default'
-set :rvm_custom_path, '/home/nurhashimah/.rvm/'  # only needed if not detected
+set :default_environment, {
+  'PATH' => "/home/nurhashimah/.rvm/gems/ruby-1.8.7-p374@catechumen/bin:/home/nurhashimah/.rvm/gems/ruby-1.8.7-p374@global/bin:/home/nurhashimah/.rvm/rubies/ruby-1.8.7-p374/bin:/home/nurhashimah/.rvm/bin:$PATH",
+  'RUBY_VERSION' => 'ruby-1.8.7-p374',
+  'GEM_HOME' => '/home/nurhashimah/.rvm/gems/ruby-1.8.7-p374@catechumen',
+  'GEM_PATH' => '/home/nurhashimah/.rvm/gems/ruby-1.8.7-p374@catechumen:/home/nurhashimah/.rvm/gems/ruby-1.8.7-p374@global' 
+}
 
 set :linked_files, fetch(:linked_files, []).push('config/database.yml')
 # set path to application
-#shared_path = "/opt/app/icms_apmm/current/shared"
+#shared_path = "/opt/app/catechumen/current/shared"
 
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
+# shall remove 'No such file or directory' error for Public subfolders
+set :normalize_asset_timestamps, false
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-pid_file = "/opt/app/catechumen/tmp/pids/server.pid"
+#pid_file = "/opt/app/catechumen/tmp/pids/server.pid"
+pid_file = "/opt/app/catechumen/current/tmp/pids/thin.pid"
 
 namespace :deploy do
-   task :start_catechumen do
-     run "cd /opt/app/catechumen/current; script/server -p 3000 -e production"
+   task :start do
+     run "cd /opt/app/catechumen/current; thin -d start -p 3000 -e production"
    end
-   task :stop_catechumen do 
+   task :stop do 
      run "kill -s QUIT `cat #{pid_file}`" if File.exists?(pid_file)
    end
    task :restart do
@@ -56,5 +51,3 @@ namespace :deploy do
 #   end
 
 end
-
-after "deploy:log_revision", "deploy:start_catechumen"
